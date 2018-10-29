13d497a1241e49f27d1212532d28d3f54a81253b
Mauricio Cerizza <mauriciocerizza@gmail.com>
2018-10-28 22:10:19 -0300

    json decode registrar empleado

--- API/EmpleadoAPI.php
+++ API/EmpleadoAPI.php
@@ -24,12 +24,12 @@ class EmpleadoApi extends Empleado
     ///Registro de nuevos empleados.
     public function RegistrarEmpleado($request, $response, $args)
     {
-        $parametros = $request->getParsedBody();
-        $usuario = $parametros["usuario"];
-        $clave = $parametros["clave"];
-        $nombre = $parametros["nombre"];
-        $tipo = $parametros["tipo"];
-
+        $json = $request->getBody();
+        $data = json_decode($json, true);
+        $usuario = $data["usuario"];
+        $clave = $data["clave"];
+        $nombre = $data["nombre"];
+        $tipo = $data["tipo"];
 
         $respuesta = Empleado::Registrar($usuario, $clave, $nombre, $tipo);
         $newResponse = $response->withJson($respuesta, 200);
