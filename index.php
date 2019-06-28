1d5d4facdad7be9c009bd62d30fe419be7dd8614
Mauricio Cerizza <mauriciocerizza@gmail.com>
2019-06-28 02:49:06 -0300

    Alta menu

--- index.php
+++ index.php
@@ -133,7 +133,6 @@ $app->post('/menu/modificar[/]', \MenuAPI::class . ':ModificarComida')
 ->add(\EmpleadoMiddleware::class . ':ValidarSocio')
 ->add(\EmpleadoMiddleware::class . ':ValidarToken'); 
 $app->get('/menu/listar[/]', \MenuAPI::class . ':ListarMenu')
-->add(\EmpleadoMiddleware::class . ':ValidarToken');   
 $app->delete('/menu/{id}[/]', \MenuAPI::class . ':BajaMenu')
 ->add(\OperacionMiddleware::class . ':SumarOperacionAEmpleado')
 ->add(\EmpleadoMiddleware::class . ':ValidarSocio')
